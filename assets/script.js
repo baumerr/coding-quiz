37e736449902df98b3bf032ba90c959bef7a2bc5
randy <baumer.randy@gmail.com>
2021-09-22 14:34:03 -0400

    set up beginTest function

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,50 @@
 var startTest = function () {
-    console.log("we have begun");
+    
+    // creating container div for the start test button
+    var mainEl = document.querySelector("#main-div")
+
+    // creating row
+    var rowEl = document.createElement("div");
+    rowEl.setAttribute("class", "row");
+    mainEl.appendChild(rowEl);
+
+    // creating center column
+    var firstColEl = document.createElement("div");
+    firstColEl.setAttribute("class", "col-12 text-center");
+    rowEl.appendChild(firstColEl);
+
+    // creating button start test
+    var startButtonEl = document.createElement("button");
+    startButtonEl.setAttribute("type", "button");
+    startButtonEl.setAttribute("class", "btn btn-primary");
+    startButtonEl.setAttribute("id", "start");
+    startButtonEl.textContent = "Start Test";
+    firstColEl.appendChild(startButtonEl);
+
+    // when startButtonEl is clicked, we begin the test
+    startButtonEl.addEventListener("click", beginTest);
+
 };
 
-startTest();
+var beginTest = function () {
 
+    console.log("start test button clicked");
 
+    // setting up first column as click the high score
+    var firstColEl = document.createElement("div");
+    firstColEl.setAttribute("class", "col-3");
+    firstColEl.setAttribute("id", "results");
 
+    var mainEl = document.querySelector("#main-div")
+    mainEl.appendChild(firstColEl);
 
+    var highScoreButtonEl = document.createElement("button");
+    highScoreButtonEl.setAttribute("type", "button");
+    highScoreButtonEl.setAttribute("class", "btn btn-link");
+    highScoreButtonEl.setAttribute("id", "viewScores");
+    highScoreButtonEl.textContent = "View High Scores";
+    firstColEl.appendChild(highScoreButtonEl);
 
-
-
-
-
-
-
-
-// var beginTest = function () {
-
-//     console.log("start test button clicked");
 
     // var countDown = setInterval(function () {
     
@@ -31,7 +58,44 @@ startTest();
     //         document.getElementById("over").innerHTML = "You have lost the game. Pack your bags and get out of here.";
     //     };
     // }, 1000);
-//}
+}
+
+startTest();
+
+{/*<div class="row text-center" id="over">
+            // View High Scores Button
+            <div class="col-3" id="results">
+                <button type="button" class="btn btn-link" id="viewScores">View High Scores</button>
+            </div>
+            //Actual Test
+            <div class="col-6" id="quiz">
+                <h1>Question Here</h1>
+                    <ul>
+                        <li>a</li>
+                        <li>b</li>
+                        <li>c</li>
+                        <li>d</li>
+                    </ul>
+                <button type="button" class="btn btn-primary" id="submit">
+                    Submit
+            </div>
+            //This shows the time left for the quiz
+            <div class="col-3" id="timer">
+                You have 90 second(s) left.
+            </div>
+        </div>*/}
+
+
+
+
+
+
+
+
+
+
+
+
 
 // var showResults = function () {
 //     console.log("you did it!");
@@ -47,7 +111,7 @@ startTest();
 
 // beginTest();
 
-//startTest.addEventListener("click", beginTest);
+
 
 // var completeTest = function () {
 
